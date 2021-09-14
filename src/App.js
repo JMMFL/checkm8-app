09ba6f273a000cab70c4ec20ff0e3a462a214e66
JMMFL <mehravarj+github@gmail.com>
2021-09-13 20:16:39 -0400

    Brainstorming player profile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 
+const ChessWebApi = require("chess-web-api")
+const chessApi = new ChessWebApi();
+
+chessApi.getPlayerMonthlyArchives("kourage")
+  .then(response => response.body)
+  .then(console.log)
+  .catch("Oops")
+
+
+
+
+// playerMonthlyArchives
+// time class 
+// white black
+
+
+// getPlayer
+// avatar,
+// name,
+// username,
+// followers,
+// country,
+// location,
+// status,
+// is_streamer,
+// twitch_url,
+// title
+
+// getPlayerStats
+// rapid rating
+// bullet rating
+// blitz rating
+// fide
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <h1>Gathering data...</h1>
   );
 }
 
