149104749447cf95f7d5c4c8eb34fa0c25a4a526
JMMFL <mehravarj+github@gmail.com>
2021-09-25 11:39:27 -0400

    Removed obsolete function

--- src/helpers.js
+++ src/helpers.js
@@ -1,17 +1,6 @@
 const ChessWebApi = require("chess-web-api")
 const chessApi = new ChessWebApi();
 
-export async function getAllPlayerData(username) {
-    let methods = [
-      "getPlayer", 
-      "getPlayerStats", 
-      "getPlayerMonthlyArchives"
-    ];
-    let promises = methods.map(method => chessApi[method](username));
-    let responses = await Promise.all(promises);
-    return responses.map(response => response.body);  
-}
-
 export function sortLadders(ladders) {
   const sortedLadders = [...ladders].sort((ladderA, ladderB) => {
       const rankA = ladderA?.last.rating;
