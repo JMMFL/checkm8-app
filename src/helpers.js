4a9b2df0e764347bda97af39fb37064fcdf4d8a8
JMMFL <mehravarj+github@gmail.com>
2021-09-25 13:07:07 -0400

    Added helper for extracting game stats

--- src/helpers.js
+++ src/helpers.js
@@ -18,4 +18,52 @@ export function sortLadders(ladders) {
   return sortedLadders
 }
 
+export async function extractStats(username, games) {
+    const stats = {
+        games: games.length,
+        abandons: 0,
+        wins: 0,
+        losses: 0,
+        checkmates: 0,
+        resigns: 0,
+        timeouts: 0,
+        draws: 0
+    }
+    
+    for (let game of games) {
+        const data = await fetch(game)
+            .then(r => r.json())
+            .then(json => json.games[0]);
+
+        const { white, black } = data;
+        const enemy = white.username !== username ? white : black;
+        const { result } = enemy;
+        if (result === "win") stats.losses++;
+        
+        switch(result) {
+            case "checkmated":
+                stats.checkmates++;
+                break;
+            case "resigned":
+                stats.resigns++;
+                break;
+            case "timeout":
+                stats.timeouts++;
+                break;
+            case 'agreed':
+                stats.draws++;
+                break;
+            case 'abandoned':
+                stats.abandons++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    stats.wins = stats.games - stats.losses - stats.draws;
+
+    return stats;
+}
+
 export default chessApi;
