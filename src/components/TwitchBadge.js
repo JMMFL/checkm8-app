720ddba490e0fd4f69ce0b1be9c465ff8453cb55
JMMFL <mehravarj+github@gmail.com>
2021-09-28 14:01:03 -0400

    Hide Twitch badge if they don't have one

--- src/components/TwitchBadge.js
+++ src/components/TwitchBadge.js
@@ -2,21 +2,20 @@ import React from "react";
 import twitchLogo from "../images/twitch.png";
 import { LargeBadge, Logo, Heading } from "./ChessBadge";
 
-
-
-
-
 function TwitchBadge({url}) {
-    return (
-        <a href={url ? url : "https://www.chess.com/streamers"} style={{textDecoration: "none", justifySelf: "end"}} target="_blank" rel="noreferrer">
-            <LargeBadge>
-                <Logo src={twitchLogo} style={{opacity: `${url ? 1 : 0.25}`}} alt="Twitch.tv logo"/>
-                <Heading>twitch.tv</Heading>
-            </LargeBadge>
-        </a>
-    )
+    if (url !== undefined) {
+        return ( 
+            <a href={url} style={{textDecoration: "none", justifySelf: "end"}} target="_blank" rel="noreferrer">
+                <LargeBadge>
+                    <Logo src={twitchLogo} alt="Twitch.tv logo"/>
+                    <Heading>twitch.tv</Heading>
+                </LargeBadge>
+            </a>
+        )
 
+    }
 
+    return <></>
 }
 
 export default TwitchBadge;
