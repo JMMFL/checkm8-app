384d99a591f6025f0461a2ab552fe200a589b288
JMMFL <mehravarj+github@gmail.com>
2021-09-29 18:22:50 -0400

    Generated match history

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
-import chessApi, { getCountryCode, getRecentStats, sortLadders } from "../helpers";
+import chessApi, { getCountryCode, getEnemyAvatars, getRecentGames, makeReview, sortLadders } from "../helpers";
 import "../App.css";
 import TitleCard from "../components/TitleCard";
 import LadderCard from "../components/LadderCard";
@@ -10,6 +10,7 @@ import CountryBadge from "../components/CountryBadge";
 import StatusBadge from "../components/StatusBadge";
 import ChessBadge from "../components/ChessBadge";
 import TwitchBadge from "../components/TwitchBadge";
+import GameCard from "../components/GameCard";
 
 const PageDiv = styled.div`
     display: flex;
@@ -67,13 +68,15 @@ const ProfileBadges = styled.div`
 
 function ProfilePage() {
     const [profile, setProfile] = useState(null);
+    const [games, setGames] = useState(null);
+    const [avatars, setAvatars] = useState(null);
     const username = "Kourage";
 
     useEffect(() => {
         async function getPlayerProfile() {
             const info = await chessApi.getPlayer(username).then(r => r.body);
             const stats = await chessApi.getPlayerStats(username).then(r => r.body);
-            const archives = await chessApi.getPlayerMonthlyArchives(username).then(r => r.body);
+            const archives = await chessApi.getPlayerMonthlyArchives(username).then(r => r.body.archives);
             
             const { followers, joined, last_online, country} = info;
             
@@ -82,24 +85,27 @@ function ProfilePage() {
             chess_bullet.mode = "bullet";
             chess_rapid.mode = "rapid";
 
-            const games = [...archives.archives].reverse().slice(0, 10);
-
+            const games = await getRecentGames(archives, 10);
+            const avatars = await getEnemyAvatars(games, username);
+            
             const profile = {
                 countryCode: await getCountryCode(country),
                 ladders: sortLadders([chess_blitz, chess_bullet, chess_rapid]),
+                review: makeReview(games, username),
                 summary: {followers, joined, last_online},
-                review: await getRecentStats(username, games),
 
                 ...info, ...stats, ...archives
             }
 
+            setGames(games);
             setProfile(profile);
+            setAvatars(avatars);
         }
 
         getPlayerProfile()
     }, [])
 
-    if (profile === null) {
+    if (profile === null || avatars === null) {
         return "Loading...";
     }
 
@@ -125,6 +131,22 @@ function ProfilePage() {
                         <ChessBadge url={profile.url} />
                         <TwitchBadge url={profile.twitch_url} />
                     </ProfileBadges>
+                </SectionDiv>
+                <SectionDiv>
+                    <SectionTitle>History</SectionTitle>
+                    {
+                        games.map((game, i) => {
+                            return (
+                                <GameCard
+                                    game={game}
+                                    username ={username}
+                                    avatar={profile.avatar}
+                                    enemyAvatar={avatars[i]}
+                                    key={i}
+                                />
+                            )
+                        })
+                    }
                 </SectionDiv>
             </ContentDiv>
         </PageDiv>
