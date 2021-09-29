c6a0f8cf7ce06ae1f4b6d9e20960ecf11ffb7bda
JMMFL <mehravarj+github@gmail.com>
2021-09-29 19:03:50 -0400

    Created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import React from "react";
+
+const Content = styled.footer`
+    background: var(--background-1);
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    width: 100vw;
+    padding-top: 10px;
+    padding-bottom: 10px;
+`
+
+const Link = styled.a`
+    color: white;
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 10px;
+    font-weight: 500;
+`
+
+function Footer() {
+
+    return (
+        <Content>
+            <Link href="https://github.com/JMMFL">Designed by Jordan</Link>
+        </Content>
+    )
+}
+
+export default Footer;
