95f455e705e56f754336b2b63636215fb9856d70
JMMFL <mehravarj+github@gmail.com>
2021-09-30 13:32:48 -0400

    Added route for profiles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<HomePage />} /> */}
-      <Route path="/" element={<ProfilePage />} />
-      {/* <Route path="*" element={<WhoopsPage />} /> */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/profile/:username" element={<ProfilePage />} />
     </Routes>
   )
 }
